6611f93b3a87fa0a28dee5cf77df1ba014660eb9
Patrick Dugan <dugan.pat@gmail.com>
2025-09-11 13:52:45 -0700

    Nit

--- lib/prompt-template-lang.ts
+++ lib/prompt-template-lang.ts
@@ -9,41 +9,41 @@ export const promptTemplateLang: LanguageRegistration = {
       begin: '\\$\\{',
       end: '\\}',
       beginCaptures: {
-        '0': { name: 'punctuation.definition.variable.begin' }
+        '0': { name: 'punctuation.definition.variable.begin' },
       },
       endCaptures: {
-        '0': { name: 'punctuation.definition.variable.end' }
+        '0': { name: 'punctuation.definition.variable.end' },
       },
       patterns: [
         {
           match: '[<>?:{}()\\[\\]]',
-          name: 'keyword.operator'
+          name: 'keyword.operator',
         },
         {
           match: '[a-zA-Z_][a-zA-Z0-9_]*',
-          name: 'support.variable'
-        }
-      ]
+          name: 'support.variable',
+        },
+      ],
     },
     header: {
       match: '^([A-Z][A-Z\\s]+):',
       captures: {
-        '1': { name: 'keyword.control' }
-      }
+        '1': { name: 'keyword.control' },
+      },
     },
     bullet: {
       match: '^\\s*-\\s+',
-      name: 'punctuation.definition.list'
+      name: 'punctuation.definition.list',
     },
     string: {
       match: '"[^"]*"',
-      name: 'string.quoted.double'
+      name: 'string.quoted.double',
     },
   },
   patterns: [
     { include: '#variable' },
     { include: '#header' },
     { include: '#bullet' },
-    { include: '#string' }
-  ]
+    { include: '#string' },
+  ],
 };
